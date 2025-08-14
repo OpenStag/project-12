ad5c3ffb9cc5c72f2c78066db0b4860c2dcf1c35
Damith Rathnayaka <damithrathnayaka0027@gmail.com>
2025-08-14 22:44:18 +0530

    create feature join team

---
+++ src/app/join-team/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='text-amber-600 font-bold text-6xl'>page</div>
+  )
+}
+
+export default page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import Link from "next/link";
 export default function Home() {
   return (
     <div>
+      <Link href='/join-team'>jpin team</Link>
       
     </div>
   );
