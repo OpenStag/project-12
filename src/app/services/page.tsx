6ee0466c91cf8822a9652980507bdb7259ae5025
janitha dissanayaka <janithadissnaykaa@gmail.com>
2025-08-14 15:34:34 +0530

    make software development division

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -3,24 +3,35 @@ import Image from "next/image"
 
 function page() {
   return (
-    <section className='p-20'>
-        <div className='flex flex-col items-left justify-center font-bold'>
-            <h1 className='text-2xl '> Our Services</h1>
-            <p>We offer a comprehensive suite if IT services designed to help businesses thrive in the digital age.From custome software development to clousde solutions and cyber security.We got you covered.</p>
+    <section className='p-20' >
+        <div className='flex flex-col items-left justify-center font-extrabold'>
+            <h1 className='text-4xl '> Our Services</h1>
+            <p className="text-gray-400 text-2xl py-6">We offer a comprehensive suite if IT services designed to help businesses thrive in the digital age.From custome software development to clousde solutions and cyber security.We got you covered.</p>
         </div>
         <div>
-            <h1>Software Development</h1>
-         <div className="flex flex-row ">
-            <div className="" >
-                <Image src="/sd1.png" alt="mobile" width={190} height={150}/>
+
+            <h1 className="font-bold text-3xl">Software Development</h1>
+            
+
+         <div className="flex flex-row  items-center justify-center p-5">
+            <div className=" flex flex-col items-center justify-center " >
+                <Image src="/sd1.png" alt="mobile" width={300} height={300} className="rounded-2xl"/>
+                
+                <p className="pt-6">Mobile Application Development</p>
+                <p className="px-35  p-6">Devloping native and cross-platform mobile apps for ios and Amdroid optimized for performance and user experience</p>
+                
 
             </div>
-            <div>
-                <Image src="/sd2.png" alt="mobile" width={250} height={250}/>
+            <div className="flex flex-col items-center justify-center p-5">
+                <Image src="/sd2.png" alt="mobile" width={300} height={300} className="rounded-2xl"/>
+                <p className="pt-6">Mobile Application Development</p>
+                <p className="px-35 p-6 ">Devloping native and cross-platform mobile apps for ios and Amdroid optimized for performance and user experience</p>
 
             </div>
-            <div>
-                <Image src="/sd3.png" alt="mobile" width={170} height={150}/>
+            <div className="flex flex-col items-center justify-center p-5 ">
+                <Image src="/sd3.png" alt="mobile" width={300} height={300} className="rounded-2xl"/>
+                <p className="pt-6">Mobile Application Development</p>
+                <p className="px-35 pt-6">Devloping native and cross-platform mobile apps for ios and Amdroid optimized for performance and user experience</p>
 
             </div>
 
