7db7778666b8678946dbd7ebbfccb9f3c2b3730b
ChamikaraD <115696292+ChamikaraD@users.noreply.github.com>
2025-08-21 01:13:44 +0530

    Add headings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
     <div>
+      <h1 className="text-pink-500"> Home </h1>
       
     </div>
   );
---
+++ src/app/solutions/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className="bg-[#163d3d] text-white min-h-screen px-6 py-12">
+        <h2 className='text-3xl font-bold'>Our Success Stories</h2>
+        <p className='mt-2 text-gray-300 '>Explore how we’ve helped businesses achieve their goals through innovative IT solutions.</p>
+        <h3 className='mt-10 font-semibold'>Featured Projects</h3>
+
+        <div className='grid md:grid-cols2 gap-6 mt-6'>
+            <div className='bg-[#d0eaeb] p-4 rounded-lg flex gap-4'>
+                
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default page
