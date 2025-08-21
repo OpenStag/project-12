3d99f73ec8585707cb2000f49d0cbcf8ebaa97c5
ChamikaraD <115696292+ChamikaraD@users.noreply.github.com>
2025-08-21 21:24:42 +0530

    Addcards

--- src/app/solutions/page.jsx
+++ src/app/solutions/page.jsx
@@ -1,4 +1,7 @@
+
 import React from 'react'
+import Image from 'next/image'
+
 
 function page() {
   return (
@@ -8,11 +11,53 @@ function page() {
         <h3 className='mt-10 font-semibold'>Featured Projects</h3>
 
         <div className='grid md:grid-cols2 gap-6 mt-6'>
-            <div className='bg-[#d0eaeb] p-4 rounded-lg flex gap-4'>
-                
+
+            <div className='bg-[#1d4d4f]  rounded-lg flex gap-4 border'>
+              <div className='flex flex-row'>
+                <Image className='inline-block'
+                src="/web page 1.png"
+                width={200}
+                height={150}
+                />
+                <div className='inline-block p-4'>
+                  <h4 className='font-bold text-lg inline-block'>E commerce Platform Optimization</h4>
+                  <p className='font-light text-amber-50'>Revamped an online retail platform, resulting in a significant boost in user engagement and a decrease in bounce rate. Increased user engagement by 40%. and reduced bounce rate by 25%                </p>
+                </div>
+              </div>
 
             </div>
+            <div className='bg-[#1d4d4f]  rounded-lg flex gap-4 border'>
+              <div className='flex flex-row'>
+                <Image
+                src="/cloud migration 1.png"
+                width={200}
+                height={150}
+                />
+                <div className='inline-block p-4'>
+                  <h4 className='font-bold text-lg'>Cloud Migration for Financial Services</h4>
+                  <p className='font-light text-amber-50'>Successfully migrated a financial institution’s infrastructure to the cloud, enhancing efficiency and cutting costs. Improved system efficiency by 30% and reduced operational costs by 15%.</p>
+                </div>
+              </div> 
+            </div>
+            <div className='bg-[#1d4d4f]  rounded-lg flex gap-4 border'>
+              <div className='flex flex-row'>
+                <Image
+                src="/cybersecurity 1.png"
+                width={200}
+                height={150}
+                />
+                <div className='p-4'>
+                  <h4 className='font-bold text-lg'>Cybersecurity Enhancement for Healthcare Provider</h4>
+                  <p className='font-light text-amber-50'>Implemented robust cybersecurity measures for a healthcare provider, ensuring data protection and systemreliability.Achieved a 99.9% uptime and improved data security by 50%.               </p>
+                </div>
+              </div>
+
+            </div>
+
+            
         </div>
+
+        <h3 className='mt-10 font-semibold'>Client Testimonials</h3>
     </div>
   )
 }
